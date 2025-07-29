0203a30d6821ee062978c6d86e6069f4dce0c437
Jonas Bird <jonas.bird.m@gmail.com>
2025-07-29 14:59:43 -0400

    feat: add display functions

--- invaders/space-invaders-starter.rkt
+++ invaders/space-invaders-starter.rkt
@@ -1,3 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname space-invaders-starter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (require 2htdp/universe)
 (require 2htdp/image)
 
@@ -18,7 +21,11 @@
 
 (define INVADE-RATE 100)
 
-(define BACKGROUND (empty-scene WIDTH HEIGHT))
+(define BACKGROUND (place-image
+                    (rectangle (* 2 WIDTH) 20 "solid" "brown")
+                    0
+                    (+ HEIGHT 10)
+                    (empty-scene WIDTH (+ HEIGHT 20))))
 
 (define INVADER
   (overlay/xy (ellipse 10 15 "outline" "blue")              ;cockpit cover
@@ -103,3 +110,98 @@
 (define G1 (make-game empty empty T1))
 (define G2 (make-game (list I1) (list M1) T1))
 (define G3 (make-game (list I1 I2) (list M1 M2) T1))
+
+
+;; =======================
+;; Functions:
+;; WS -> WS
+;; start the world with ...
+;;
+(define (main ws)
+  (big-bang ws                 ;WS
+    (on-tick tock)     ;WS->WS
+    (to-draw render-game)   ;WS->Image
+    (stop-when ...)    ;WS->Boolean
+    ; (on-mouse ...)     ;WS Integer Integer MouseEvent -> WS
+    (on-key ...)))     ;WS KeyEvent->WS
+
+;; Game -> Game
+;; produce the next ...
+;; !!!
+(define (tock ws) ...)
+
+;; WS -> Image
+;; render ...
+;; !!!
+(define (render-game s)
+  (... (render-loinvader (game-invaders s))
+       (render-lom (game-missiles s))
+       (render-tank (game-tank s) BACKGROUND)))
+
+;; List-of-invaders Image -> Image
+;; render the list of UFOs
+;; !!!
+(check-expect (render-loinvader '() BACKGROUND) BACKGROUND)
+(check-expect
+ (render-loinvader (list (make-invader 100 100 10)) BACKGROUND)
+ (place-image INVADER 100 100 BACKGROUND))
+(check-expect
+ (render-loinvader (list I1 I2 I3) BACKGROUND)
+ (place-images
+  (list INVADER INVADER INVADER)
+  (list (make-posn (invader-x I1) (invader-y I1))
+        (make-posn (invader-x I2) (invader-y I2))
+        (make-posn (invader-x I3) (invader-y I3))) BACKGROUND))
+;(define (render-loinvader loi im) im) ;stub
+
+(define (render-loinvader loi im)
+  (cond [(empty? loi) BACKGROUND]
+        [else
+         (place-image INVADER
+                      (invader-x (first loi))
+                      (invader-y (first loi))
+                      (render-loinvader (rest loi) BACKGROUND))]))
+
+;; List-of-missiles Image -> Image
+;; render the list of Missiles
+(check-expect (render-lom '() BACKGROUND) BACKGROUND)
+(check-expect (render-lom (list (make-missile 100 100)) BACKGROUND)
+              (place-image MISSILE 100 100 BACKGROUND))
+(check-expect (render-lom (list (make-missile 100 100)
+                                (make-missile 120 100)
+                                (make-missile 50 50))
+                          BACKGROUND)
+              (place-images (make-list 3 MISSILE)
+                            (list (make-posn 100 100)
+                                  (make-posn 120 100)
+                                  (make-posn 50 50))
+                            BACKGROUND))
+;(define (render-lom lom im) im) ;stub
+
+(define (render-lom lom im)
+  (place-images (make-list (length lom) MISSILE) (map missile->posn lom) im))
+
+;; Missile -> Posn
+;; make a list of missiles a list of posn
+(define (missile->posn m)
+  (make-posn (missile-x m) (missile-y m)))
+
+;; Tank Image -> Image 
+;; adds t to the given image im
+(check-expect (render-tank T0 BACKGROUND)
+              (place-image TANK (tank-x T0)  (- HEIGHT  TANK-HEIGHT/2) BACKGROUND))
+(check-expect (render-tank T1 BACKGROUND)
+              (place-image TANK (tank-x T1)  (- HEIGHT  TANK-HEIGHT/2) BACKGROUND))
+;(define (render-tank t im) im) ;stub
+
+(define (render-tank t im)
+  (place-image TANK (tank-x t) (- HEIGHT  TANK-HEIGHT/2) im))
+
+
+
+
+;; Game Key -> Game
+;; handle player input
+(define (handle-key ws ke)
+  (cond [(key=? ke " ") (... ws)]
+        [else (... ws)]))
